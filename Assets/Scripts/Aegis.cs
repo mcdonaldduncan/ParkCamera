b3f5ef843ed246228faecb7fb1a02e48148d8ad2
mcdonaldduncan <380939@colum.edu>
2022-09-08 18:52:59 -0500

    add aegis

---
+++ Assets/Scripts/Aegis.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aegis : MonoBehaviour
+{
+    enum AgeState
+    {
+        Child,
+        Teen,
+        Adult,
+        Elderly,
+        Dead
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
